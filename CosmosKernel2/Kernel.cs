80e3b52ffb3ac127e7d2ca6afdb2371ca4224f15
Dylan <Dylan@DESKTOP-S7SME1P>
2021-12-08 19:34:22 -0600

    Add project files.

---
+++ CosmosKernel2/Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace PogOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Loading PogOS..");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("$: ");
+            var Input = Console.ReadLine().Split(" ");
+
+        }
+    }
+}
