9330a34d9390834a4bd1920b264b7834df223db0
Dylstructor <38115495+poggingfish@users.noreply.github.com>
2021-12-08 23:18:17 -0600

    Added username

--- CosmosKernel2/Kernel.cs
+++ CosmosKernel2/Kernel.cs
@@ -7,17 +7,22 @@ namespace PogOS
 {
     public class Kernel : Sys.Kernel
     {
-        public static String PogVer = "1.0";
+        public static String PogVer = "1.1";
+        public static String Username = "root";
         protected override void BeforeRun()
         {
+            
             Console.Clear();
             Console.WriteLine("Loading PogOS..");
         }
 
         protected override void Run()
         {
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(Username);
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("PogOS $: ");
+            Console.Write("@PogOS $: ");
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             var Input = Console.ReadLine().Split(" ");
             if (Input[0].ToLower() == "print")
