f050c41196d9e93d9677f0f4932de0bbd717947c
Dylstructor <38115495+poggingfish@users.noreply.github.com>
2021-12-08 20:55:07 -0600

    Updated alot.

--- CosmosKernel2/Kernel.cs
+++ CosmosKernel2/Kernel.cs
@@ -7,25 +7,36 @@ namespace PogOS
 {
     public class Kernel : Sys.Kernel
     {
+        public static String PogVer = "1.0";
         protected override void BeforeRun()
         {
+            
             Console.WriteLine("Loading PogOS..");
         }
 
         protected override void Run()
         {
-            Console.Write("$: ");
+            Console.Write("PogOS $: ");
             var Input = Console.ReadLine().Split(" ");
             if(Input[0].ToLower() == "print")
             {
                 try
                 {
-                    Console.WriteLine(Input[1]);
+                    foreach(String x in Input)
+                    {
+                        if (x != "print"){Console.Write(x);}
+                    }
+                    Console.WriteLine("");
                 }
                 catch
                 {
                     Console.WriteLine("Error.");
                 }
+            }
+
+            if (Input[0].ToLower() == "sysinfo")
+            {
+                Console.WriteLine("PogOS Version: " + PogVer);
             }
         }
     }
