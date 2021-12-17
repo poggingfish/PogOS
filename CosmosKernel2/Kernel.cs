8acdc914922d313cf1dbf0e56e498cd2fb45f62d
Dylstructor <38115495+poggingfish@users.noreply.github.com>
2021-12-16 21:55:27 -0600

    did this stuff a while ago. i forgor

--- CosmosKernel2/Kernel.cs
+++ CosmosKernel2/Kernel.cs
@@ -5,20 +5,19 @@ using Sys = Cosmos.System;
 
 namespace PogOS
 {
-    public class Kernel : Sys.Kernel
-    {
-        public static String PogVer = "1.1";
+    public class Kernel : Sys.Kernel{
+        public static String PogVer = "1.2";
         public static String Username = "";
         protected override void BeforeRun()
         {
-            
-            Console.Clear();
+            Console.Clear();            
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Loading PogOS..");
+            Console.WriteLine("Loading PogOS "+PogVer+".");
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("What is your name: ");
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Username = Console.ReadLine();
+            
         }
 
         protected override void Run()
@@ -45,24 +44,29 @@ namespace PogOS
                 }
             }
 
-            if (Input[0].ToLower() == "sysinfo")
+            else if(Input[0].ToLower() == "sysinfo")
             {
                 Console.WriteLine("PogOS Version: " + PogVer);
             }
-            if (Input[0].ToLower() == "cls") {
+            else if (Input[0].ToLower() == "cls" || Input[0].ToLower() == "clear") {
                 Console.Clear();
             }
-            if (Input[0].ToLower() == "changename")
+            else if (Input[0].ToLower() == "changename")
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.Write("What is your name: ");
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Username = Console.ReadLine();
             }
-            if (Input[0].ToLower() == "date")
+            else if (Input[0].ToLower() == "date")
             {
                 var time = DateTime.Now;
                 Console.WriteLine(time);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unknown command.");
             }
 
         }
